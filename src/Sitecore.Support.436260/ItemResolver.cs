a225e8b7b0614481f97aa39d4b55e47104357e39
Vladimir Taran <vlt@sitecore.net>
2018-04-30 16:57:40 -0400

    A wrong item was resolved when processing specific URLs, and the Layout Not Found page was shown even when the item did not exist.

---
+++ src/Sitecore.Support.436260/ItemResolver.cs
@@ -0,0 +1,28 @@
+﻿using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Pipelines.HttpRequest;
+
+namespace Sitecore.Support.Pipelines.HttpRequest
+{
+  public class ItemResolver : Sitecore.Pipelines.HttpRequest.ItemResolver
+  {
+    public override void Process(HttpRequestArgs args)
+    {
+      base.Process(args);
+
+      Database db = Context.Database;
+      if (db == null)
+      {
+        return;
+      }
+      Item root = db.GetRootItem();
+      if (Context.Item != null && root != null)
+      {
+        if (Context.Item.Paths.FullPath == root.Paths.FullPath)
+        {
+          Context.Item = null;
+        }
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.436260/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.436260")]
+[assembly: AssemblyProduct("Sitecore.Support.436260")]
+[assembly: ComVisible(false)]
